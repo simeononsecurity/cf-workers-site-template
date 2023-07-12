9161ee68efc63b3c2ca7ec78fb0dbdf8620e683b
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-07-12 01:12:12 +0000

    Update handler.ts

--- src/handler.ts
+++ src/handler.ts
@@ -4,15 +4,20 @@ import { proxies } from "./config/proxies";
 
 export async function handleEvent(event: FetchEvent) {
   const url: URL = new URL(event.request.url);
+  const originalHeaders = event.request.headers;
 
   for (const proxy of proxies) {
     if (url.pathname.startsWith(proxy.prefix)) {
       // Proxy request to the target URL and include original request headers
+      const headers = new Headers();
+      originalHeaders.forEach((value, key) => {
+        headers.append(key, value);
+      });
+
       const response = await fetch(
         proxy.target + url.pathname.substr(proxy.prefix.length),
         {
-          ...event.request,
-          headers: new Headers(event.request.headers)
+          headers: headers
         }
       );
       return response;
@@ -21,18 +26,26 @@ export async function handleEvent(event: FetchEvent) {
 
   try {
     // Fetch asset from Key-Value store and include original request headers
+    const headers = new Headers();
+    originalHeaders.forEach((value, key) => {
+      headers.append(key, value);
+    });
+
     return await getAssetFromKV(event, {
-      headers: {
-        ...event.request.headers
-      }
+      headers: headers
     });
   } catch (ex) {
     if (ex.status === 404) {
       // Page not found. Do a 404 fallback for SPA and include original request headers
+      const headers = new Headers();
+      originalHeaders.forEach((value, key) => {
+        headers.append(key, value);
+      });
+
       return await getAssetFromKV(event, {
         mapRequestToAsset: (req) => {
           return new Request(`${new URL(req.url).origin}/index.html`, {
-            headers: new Headers(req.headers)
+            headers: headers
           });
         }
       });
