53161b6a130a5d49698e94479da83368897552fa
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-07-12 02:33:37 +0000

    Update handler.ts

--- src/handler.ts
+++ src/handler.ts
@@ -15,18 +15,33 @@ export async function handleEvent(event: FetchEvent) {
   }
 
   try {
-    // Pass the original request headers to getAssetFromKV
-    return await getAssetFromKV(event, {
-      headers: event.request.headers,
+    const originalResponse = await getAssetFromKV(event, {});
+
+    // Retrieve original server response headers
+    const originalHeaders = originalResponse.headers;
+
+    // Clone the original response to preserve its body
+    const clonedResponse = new Response(originalResponse.body, originalResponse);
+
+    // Retrieve headers from the KV response
+    const kvHeaders = clonedResponse.headers;
+
+    // Merge the headers from the original server response and the KV response
+    const mergedHeaders = new Headers([...kvHeaders, ...originalHeaders]);
+
+    // Create a new response with the merged headers
+    const responseWithMergedHeaders = new Response(clonedResponse.body, {
+      ...clonedResponse,
+      headers: mergedHeaders,
     });
+
+    return responseWithMergedHeaders;
   } catch (ex) {
     if (ex.status === 404) {
       // Page not found. Do a 404 fallback for SPA
       return await getAssetFromKV(event, {
         mapRequestToAsset: (req) => {
-          return new Request(`${new URL(req.url).origin}/index.html`, {
-            headers: req.headers, // Pass the original request headers
-          });
+          return new Request(`${new URL(req.url).origin}/index.html`);
         },
       });
     }
