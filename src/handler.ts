65d25c82d011f4991939d2c45f54d523ecbac82e
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-07-12 00:51:34 +0000

    Update handler.ts

--- src/handler.ts
+++ src/handler.ts
@@ -7,22 +7,34 @@ export async function handleEvent(event: FetchEvent) {
 
   for (const proxy of proxies) {
     if (url.pathname.startsWith(proxy.prefix)) {
-      return await fetch(
+      // Proxy request to the target URL and include original request headers
+      const response = await fetch(
         proxy.target + url.pathname.substr(proxy.prefix.length),
-        event.request
+        {
+          ...event.request,
+          headers: new Headers(event.request.headers)
+        }
       );
+      return response;
     }
   }
 
   try {
-    return await getAssetFromKV(event, {});
+    // Fetch asset from Key-Value store and include original request headers
+    return await getAssetFromKV(event, {
+      headers: {
+        ...event.request.headers
+      }
+    });
   } catch (ex) {
     if (ex.status === 404) {
-      // Page not found. Do a 404 fallback for SPA
+      // Page not found. Do a 404 fallback for SPA and include original request headers
       return await getAssetFromKV(event, {
         mapRequestToAsset: (req) => {
-          return new Request(`${new URL(req.url).origin}/index.html`);
-        },
+          return new Request(`${new URL(req.url).origin}/index.html`, {
+            headers: new Headers(req.headers)
+          });
+        }
       });
     }
 
